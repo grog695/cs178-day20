42381b7a961f63a876a798314f1bcf5e0b58adbf
George <george.angelakis@outlook.com>
2026-04-16 12:00:34 -0500

    Text Change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<h2>Here is an interesting xkcd cartoon for you: </h2>" \
+           "<h2>Here is a picture of the Bez Mbeng rookie card: </h2>" \
            '<img src="https://cdn.shopify.com/s/files/1/0697/4249/3893/files/6c2a15bc59c706e2a5da7ebeb4050a8a995ef5c3_ARTBK_16C2S_26TN_0251.jpg?v=1775931609">'
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
