11155ee1d29376fc9f5741fe34be3c19a93cda25
George <george.angelakis@outlook.com>
2026-04-16 11:58:52 -0500

    New Picture

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<h2>Here is an interesting xkcd cartoon for you: </h2>" \
-           '<img src="c:\Users\Georg\OneDrive\Pictures\Screenshots\Screenshot 2026-04-14 203513.png">'
+           '<img src="https://cdn.shopify.com/s/files/1/0697/4249/3893/files/6c2a15bc59c706e2a5da7ebeb4050a8a995ef5c3_ARTBK_16C2S_26TN_0251.jpg?v=1775931609">'
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
